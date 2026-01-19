faf6130105d673d9d9effef96f6e4c5b8054d146
Fabian Seitz <github@fabiseitz.de>
2026-01-19 10:49:39 +0100

    Update config_flow.py

--- custom_components/whatsapp/config_flow.py
+++ custom_components/whatsapp/config_flow.py
@@ -10,7 +10,6 @@ from typing import Any
 
 import voluptuous as vol
 from homeassistant import config_entries
-
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
